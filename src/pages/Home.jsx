0926f37dda9a3595c0e92fcd77c2a8c84ec949a1
Issak <issakthomas22@gmail.com>
2025-03-16 21:19:10 +0530

    V1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,19 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <div className="name">
-        <h1>Issak Thomas</h1>
+      <div
+        className="name animate__animated animate__fadeIn"
+        data-aos="fade-right"
+      >
+        <h1>ISSAK THOMAS</h1>
         <span>Full Stack Developer | MERN Stack Intern</span>
       </div>
-      <div className="links">
+      <div className="links animate__animated animate__fadeIn">
         <Link className="link" to={"/about"}>
           ABOUT
+        </Link>
+        <Link className="link" to={"/about"}>
+          EXPERIENCE
         </Link>
         <Link className="link" to={"/projects"}>
           PROJECTS
